f7bfc757a9dff2f7713c0e93a5809bef2b2bd478
Rai Singh <rsingh@debbie.rgsinfotech.local>
2013-11-02 22:30:57 -0400

    s

--- src/main/java/com/example/controller/PersonController.java
+++ src/main/java/com/example/controller/PersonController.java
@@ -33,7 +33,7 @@ public class PersonController {
 
         personService.addPerson(person);
 
-        return "redirect:people/people";
+        return "people/people";
     }
 
     @RequestMapping("/delete/{personId}")
@@ -41,6 +41,6 @@ public class PersonController {
 
         personService.removePerson(personId);
 
-        return "redirect:people/people";
+        return "people/people";
     }
 }
